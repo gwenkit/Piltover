e0fe3aab65a3e14dc4870b9b630d6a46e73ca52f
gwenkit.com <build@gwenkit.com>
2024-10-08 22:38:33 +0900

    Hello, world!

---
+++ Arduino_Mega_2560_Rev3/Arduino_Mega_2560_Rev3_first_look/Arduino_Mega_2560_Rev3_first_look.ino
@@ -0,0 +1,11 @@
+// put your setup code here, to run once:
+void setup() {
+  Serial.begin(9600);
+  delay(1000 * 4);
+  Serial.println("Hello, world!");
+}
+
+
+// put your main code here, to run repeatedly:
+void loop() {
+}
